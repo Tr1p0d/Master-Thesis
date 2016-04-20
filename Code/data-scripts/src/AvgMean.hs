278a9d2f5965d7491b05077882d3e3c9e8717f69
Marek 'Tr1p0d' Kidon <marek.kidon@itcommunity.cz>
2016-04-20 21:23:26 +0200

    Generalized AvgMean

--- Code/data-scripts/src/AvgMean.hs
+++ Code/data-scripts/src/AvgMean.hs
@@ -12,7 +12,7 @@ import qualified Data.Matrix as M (Matrix, fromLists, getRow, nrows, transpose, 
 import System.FilePath.Posix ((</>))
 import Statistics.Quantile (continuousBy, medianUnbiased)
 
-import SimpleStatisticsParser 
+import SimpleStatisticsParser
     ( Fitness(Fitness)
     , Generation
     , Hits
@@ -26,13 +26,15 @@ import SimpleStatisticsParser
 
 main :: IO ()
 main = do
-    (targetDir:_) <- getArgs
+    (targetDir:qCountS:qS:[]) <- getArgs
+    let qCount = read qCountS :: Int
+        q = read qS :: Int
     (statFiles) <- listDirectory targetDir
     stats <- M.fromLists <$> mkRunMatrix targetDir statFiles
-    printTikz $ computeMedian $ M.transpose stats
+    printTikz ("generation", "hits") $ computeMedian qCount q $ M.transpose stats
   where
     mkRunMatrix target = mapM (vectorProgress <=< handleError <=< parseSimpleStatistics . (target </>))
-    computeMedian = rowWise (continuousBy medianUnbiased 1 2) 
+    computeMedian count q = rowWise (continuousBy medianUnbiased q count)
     vectorProgress = return . map (value . snd) . progress
     handleError = either reportError return
     reportError e = do
@@ -42,8 +44,7 @@ main = do
 rowWise :: (V.Vector a -> b) -> M.Matrix a -> [b]
 rowWise op mat = map (\rowN -> op (M.getRow rowN mat)) [1 .. M.nrows mat]
 
-
-printTikz :: [Double] -> IO ()
-printTikz = void . zipWithM (\g f -> putStrLn $ show (g, (1-f) * 8192)) [0..]
+printTikz :: (String, String) -> [Double] -> IO ()
+printTikz a = void . zipWithM (\g f -> putStrLn $ show (g, (1-f) * 8192)) [0..]
   where
-    showGenerationF (g, Fitness{..}) = show $ (g, hits) 
+    showGenerationF (g, Fitness{..}) = show $ (g, hits)
